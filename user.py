62549d0537aeffb4d38183e39eae2a3006e55a6a
Tony Kihara <tonykiharatonkin@gmail.com>
2019-06-17 01:55:09 +0300

    defined the objects of the class

--- user.py
+++ user.py
@@ -6,13 +6,13 @@ class User:
     Class to create user accounts and save their credentials
     '''
     users_list = []
-#     def __init__(self,first_name,last_name,password):
-#         '''
-#         Methods to define each user object properties
-#         '''
-#         self.first_name = first_name
-#         self.last_name = last_name
-#         self.password = password
+    def __init__(self,first_name,last_name,password):
+        '''
+        Methods to define each user object properties
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
 #     def save_user(self):
 #         '''
 #         Function to save a new user
